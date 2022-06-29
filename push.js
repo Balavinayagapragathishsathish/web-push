0549e9814d9e850863c6a437616d2559541044c0
balavinayagapragathish.sathish <balavinayagapragathish.sathish@zoomrx.com>
2022-06-29 20:44:27 +0530

    Added key

--- push.js
+++ push.js
@@ -8,5 +8,5 @@ let vapidKeys = {
 
 push.setVapidDetails('mailto:test@code.com', vapidKeys.publicKey, vapidKeys.privateKey);
 
-let sub = {};
+let sub = {"endpoint":"https://fcm.googleapis.com/fcm/send/dl_1l8-4RGA:APA91bHDvT5JgcNMPkM2sbrlLOCaTw08tQtbEB5sPp-AUjv3qH028PFG5Kj_rp4nkWxmK5Vv12B3pqXlBd3PwtGOcNmSHjHqCRG1ZVgpQoEDnjbYtZ-4gQh3wCSNmvyj3DnY8P9cy06u","expirationTime":null,"keys":{"p256dh":"BPOLkpFgLFGbmXsZkoM89k3eV2becDmeqaBurR_Pq3OlHuTEdwPCeMpBVZ7ARd9XywVq2i9ihBSZpnz7ib0nfFk","auth":"Sehpit2dwFg-GFFR72IoXg"}}
 push.sendNotification(sub, 'test message');
